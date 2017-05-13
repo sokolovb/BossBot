28e13597e164b2fe91d531b0ff470d7127cc06a3
sokolovb <bob.sokolov.mipt@gmail.com>
2017-05-13 20:41:52 +0300

    Initial commit

---
+++ bossbot.py
@@ -0,0 +1,21 @@
+import sys
+import time
+import telepot
+from telepot.loop import MessageLoop
+
+def handle(msg):
+    content_type, chat_type, chat_id = telepot.glance(msg)
+    print(content_type, chat_type, chat_id)
+
+    if content_type == 'text':
+        bot.sendMessage(chat_id, 'Привет, ' + msg['chat']['first_name'])
+
+TOKEN = '300052001:AAE6fdytbvLS9Za1w4RjYPibVQQe6URGKYg'
+
+bot = telepot.Bot(TOKEN)
+MessageLoop(bot, handle).run_as_thread()
+print('Listening ...')
+
+# Keep the program running.
+while 1:
+    time.sleep(10)
