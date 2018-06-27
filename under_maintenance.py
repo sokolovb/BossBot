43b0db85f55a510ac6df0aebf161806c9393f284
Boris <bob-sokolov@mail.ru>
2018-06-27 21:29:36 +0300

    Update under_maintenance.py

--- under_maintenance.py
+++ under_maintenance.py
@@ -8,7 +8,7 @@ def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     bot.sendMessage(chat_id, 'Sorry, BossBot is under maintenance for a while')
 
-TOKEN = '300052001:AAE6fdytbvLS9Za1w4RjYPibVQQe6URGKYg'
+TOKEN = 'your_token_here'
 
 bot = telepot.Bot(TOKEN)
 MessageLoop(bot, handle).run_as_thread()
